73d015645ee96ccbf462ed665b2f57d4aadf79f4
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-07-28 21:11:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import "./App.css";
+import SearchBar from "./SearchBar";
+import Location from "./Location";
+import City from "./City";
+import Date from "./Date";
+import Icon from "./Icon";
+import Temp from "./Temp";
+import Conditions from "./Conditions";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <div className="container">
+        <div className="weather-app">
+          <div className="row justify-content-md-center">
+            <div className="col-8 col-md-10">
+              <SearchBar />
+            </div>
+            <div className="col-2 col-md-2">
+              <Location />
+            </div>
+          </div>
+          <City />
+          <Date />
+          <div className="row">
+            <div className="col-6 col-sm-4 col-md-3 main-icon">
+              <Icon />
+            </div>
+            <div className="col-6 col-sm-8 col-md-9">
+              <Temp />
+              <Conditions />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
