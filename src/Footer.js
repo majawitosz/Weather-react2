a78fea33794f4f8b2adb626d432a2356f485012e
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-07-28 21:11:14 +0200

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import "./App";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <a
+          href="https://github.com/majawitosz/vanilla-weather-app"
+          class="github-link"
+        >
+          Open-source code
+        </a>
+        by Maja Witosz 👩‍💻
+      </footer>
+    </div>
+  );
+}
