1947a19b33295cb3395125059a2d85ef53f0fd81
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-07-28 21:11:16 +0200

    Create Icon.js

---
+++ src/Icon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App";
+
+export default function Icon() {
+  return (
+    <div className="Icon">
+      <img
+        src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/sun-behind-cloud_26c5.png"
+        alt="sun"
+      />
+    </div>
+  );
+}
