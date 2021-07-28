3790578bf8d5d4371cc0e3924860144cd54e4554
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-07-28 21:11:09 +0200

    Create Conditions.js

---
+++ src/Conditions.js
@@ -0,0 +1,19 @@
+import "./App";
+
+export default function Conditions() {
+  return (
+    <div className="Conditions">
+      <ul class="conditions">
+        <li>
+          Humidity: <span>80</span>%
+        </li>
+        <li>
+          Wind: <span>5</span> m/h
+        </li>
+        <li>
+          Air pressure: <span>1080</span> hPa
+        </li>
+      </ul>
+    </div>
+  );
+}
