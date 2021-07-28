4fdb908bc41ac8645fa80d21e4b3b73b35bce0b0
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-07-28 21:11:12 +0200

    Create Date.js

---
+++ src/Date.js
@@ -0,0 +1,10 @@
+import "./App";
+
+export default function Date() {
+  return (
+    <div className="Date">
+      <p>Last updated at:</p>
+      <span className="current-time">Monday 16:24</span>
+    </div>
+  );
+}
