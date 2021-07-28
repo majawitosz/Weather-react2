7782801aa70a10ef9648bb9268c22bfb7b14533a
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-07-28 21:11:24 +0200

    Create Temp.js

---
+++ src/Temp.js
@@ -0,0 +1,10 @@
+import "./App";
+
+export default function Temp() {
+  return (
+    <div className="Temp">
+      <h2 className="temp">18</h2>
+      <span className="degrees"> °C </span>
+    </div>
+  );
+}
