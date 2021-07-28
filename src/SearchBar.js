61276687863e3e86e2b999a873cd30f56b4470fa
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-07-28 21:02:59 +0200

    Create SearchBar.js

---
+++ src/SearchBar.js
@@ -0,0 +1,25 @@
+import "./App";
+
+export default function () {
+  return (
+    <div>
+      <form>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter a city..."
+            aria-label="City"
+            aria-describedby="city"
+            autocomplete="off"
+          />
+          <input
+            className="btn btn-outline-secondary"
+            type="submit"
+            value="Search"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
