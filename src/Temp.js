931e43c666c2d5fd9ea70b004cb6a309077566a0
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-08-24 10:04:01 +0200

    Update Temp.js

--- src/Temp.js
+++ src/Temp.js
@@ -1,10 +1,26 @@
 import "./App";
+import "./Temp.css";
 
 export default function Temp() {
   return (
-    <div className="Temp">
-      <h2 className="temp">18</h2>
-      <span className="degrees"> °C </span>
+    <div>
+      <div className="Temp">
+        <h2 className="temp">18</h2>
+        <span className="degrees"> °C </span>
+      </div>
+      <div>
+        <ul className="conditions">
+          <li>
+            Humidity: <span>80</span>%
+          </li>
+          <li>
+            Wind: <span>5</span> m/h
+          </li>
+          <li>
+            Air pressure: <span>1080</span> hPa
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
