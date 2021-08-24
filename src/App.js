bfd6abbb3ae13197986f701aea972d62b8681275
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-08-24 10:03:17 +0200

    changin compononent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
 import SearchBar from "./SearchBar";
-import Location from "./Location";
-import City from "./City";
 import Date from "./Date";
 import Icon from "./Icon";
 import Temp from "./Temp";
-import Conditions from "./Conditions";
 import Footer from "./Footer";
 
 function App() {
@@ -13,15 +10,7 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="weather-app">
-          <div className="row justify-content-md-center">
-            <div className="col-8 col-md-10">
-              <SearchBar />
-            </div>
-            <div className="col-2 col-md-2">
-              <Location />
-            </div>
-          </div>
-          <City />
+          <SearchBar />
           <Date />
           <div className="row">
             <div className="col-6 col-sm-4 col-md-3 main-icon">
@@ -29,7 +18,6 @@ function App() {
             </div>
             <div className="col-6 col-sm-8 col-md-9">
               <Temp />
-              <Conditions />
             </div>
           </div>
         </div>
