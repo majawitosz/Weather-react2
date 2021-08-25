37e670444621667cf4e66b390b1da28c5f7be586
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-08-25 14:22:22 +0200

    Update Date.js

--- src/Date.js
+++ src/Date.js
@@ -3,9 +3,11 @@ import "./Date.css";
 
 export default function Date() {
   return (
-    <div className="Date">
-      <p>Last updated at:</p>
-      <span className="current-time">Monday 16:24</span>
+    <div className="container">
+      <div className="Date">
+        <p>Last updated at:</p>
+        <span className="current-time">Monday 16:24</span>
+      </div>
     </div>
   );
 }
