cb5059927d072a5d86381fdcc186a07b8ebf389d
majawitosz <79769637+majawitosz@users.noreply.github.com>
2021-08-25 14:22:24 +0200

    Update Icon.js

--- src/Icon.js
+++ src/Icon.js
@@ -3,11 +3,13 @@ import "./App";
 
 export default function Icon() {
   return (
-    <div className="Icon">
-      <img
-        src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/sun-behind-cloud_26c5.png"
-        alt="sun"
-      />
+    <div className="container">
+      <div className="Icon">
+        <img
+          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/sun-behind-cloud_26c5.png"
+          alt="sun"
+        />
+      </div>
     </div>
   );
 }
